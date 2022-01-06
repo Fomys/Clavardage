68aef1b3c102e34248f0ae3876832d034ec28eda
Louis Chauvet <louis.chauvet@free.fr>
2022-01-06 11:53:18 +0100

    Vérification du pseudo

--- src/main/java/gui/MainWindow.java
+++ src/main/java/gui/MainWindow.java
@@ -3,6 +3,7 @@ package gui;
 import database.Database;
 import database.DatabaseObserver;
 import database.Message;
+import diffusion.Diffusion;
 import gui.composants.MainPanel;
 import gui.composants.PopUpJava;
 import messages.MessageServer;
@@ -15,19 +16,21 @@ import javax.swing.*;
 public class MainWindow {
 
     private final Database database;
+    private final Diffusion diffusion;
     private JFrame main_frame;
     private MainPanel main_panel;
     private MessageServer message_server ; 
 
-    public MainWindow(Database database, MessageServer message_server) throws IOException {
+    public MainWindow(Database database, MessageServer message_server, Diffusion diffusion) throws IOException {
         this.database = database;
-        this.message_server = message_server; 
+        this.message_server = message_server;
+        this.diffusion = diffusion;
         this.build();
     }
 
     private void build() throws IOException {
         this.main_frame = new JFrame("Clavardage");
-        this.main_panel = new MainPanel(this.database, this.message_server);
+        this.main_panel = new MainPanel(this.database, this.message_server, this.diffusion);
         this.main_frame.getContentPane().add(this.main_panel);
         this.main_frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         this.main_frame.pack();
@@ -35,7 +38,7 @@ public class MainWindow {
         this.main_panel.setDoubleBuffered(true);
         this.main_frame.setVisible(true);
 
-        new PopUpJava(database);
+        new PopUpJava(this.database, this.diffusion);
     }
 }
 
